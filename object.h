fa991c6d02c2277ee83fef388e38394f4383e94d
Clément Couture <clement.couture@beatfitnessgames.com>
2020-12-04 11:56:19 +0100

    Initial commit

---
+++ object.h
@@ -0,0 +1,34 @@
+//
+//  Framework for a raytracer
+//  File: object.h
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#ifndef OBJECT_H_AXKLE0OF
+#define OBJECT_H_AXKLE0OF
+
+#include "triple.h"
+#include "light.h"
+#include "material.h"
+
+
+class Object {
+public:
+    Material material;
+
+    virtual Hit intersect(const Ray &ray) = 0;
+
+    virtual ~Object() = default;
+};
+
+#endif /* end of include guard: OBJECT_H_AXKLE0OF */
---
+++ scene.cpp
@@ -0,0 +1,94 @@
+//
+//  Framework for a raytracer
+//  File: scene.cpp
+//
+//  Created for the Computer Science course "Introduction Computer Graphics"
+//  taught at the University of Groningen by Tobias Isenberg.
+//
+//  Authors:
+//    Maarten Everts
+//    Jasper van de Gronde
+//
+//  This framework is inspired by and uses code of the raytracer framework of 
+//  Bert Freudenberg that can be found at
+//  http://isgwww.cs.uni-magdeburg.de/graphik/lehre/cg2/projekt/rtprojekt.html 
+//
+
+#include <algorithm>
+#include "scene.h"
+#include "material.h"
+
+
+Color Scene::trace(const Ray &ray)
+{
+    // Find hit object and distance
+    Hit min_hit(std::numeric_limits<double>::infinity(),Vector());
+
+    auto hit_iterator = std::min_element(
+            std::begin(objects), std::end(objects),
+            [&ray](const std::unique_ptr<Object>& o1, const std::unique_ptr<Object>& o2)
+            { return Hit(o1->intersect(ray)).t < Hit(o2->intersect(ray)).t; });
+
+    // No hit? Return background color.
+    if (Hit((*hit_iterator)->intersect(ray)) == Hit::NO_HIT()) return Color(0.0, 0.0, 0.0);
+
+    std::unique_ptr<Object>& obj = *hit_iterator;
+
+    Material& material = obj->material;            //the hit objects material
+    Point hit = ray.at(min_hit.t);                 //the hit point
+    Vector N = min_hit.N;                          //the normal at hit point
+    Vector V = -ray.D;                             //the view vector
+
+
+    /****************************************************
+    * This is where you should insert the color
+    * calculation (Phong model).
+    *
+    * Given: material, hit, N, V, lights[]
+    * Sought: color
+    *
+    * Hints: (see triple.h)
+    *        Triple.dot(Vector) dot product
+    *        Vector+Vector      vector sum
+    *        Vector-Vector      vector difference
+    *        Point-Point        yields vector
+    *        Vector.normalize() normalizes vector, returns length
+    *        double*Color        scales each color component (r,g,b)
+    *        Color*Color        dito
+    *        pow(a,b)           a to the power of b
+    ****************************************************/
+
+    Color color = material.color;                  // place holder
+
+    return color;
+}
+
+void Scene::render(Image &img)
+{
+    int w = img.width();
+    int h = img.height();
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            Point pixel(x+0.5, h-1-y+0.5, 0);
+            Ray ray(eye, (pixel-eye).normalized());
+            Color col = trace(ray);
+            col.clamp();
+            img(x,y) = col;
+        }
+    }
+}
+
+void Scene::addObject(std::unique_ptr<Object>&& o)
+{
+    objects.push_back(std::move(o));
+}
+
+void Scene::addLight(std::unique_ptr<Light>&& l)
+{
+    lights.push_back(std::move(l));
+}
+
+void Scene::setEye(Triple e)
+{
+    eye = e;
+}
