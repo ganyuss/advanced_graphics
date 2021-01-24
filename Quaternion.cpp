b41dc67d02739ffde21d0d9ad961ef13233ed4b6
Clément Couture <clement.couture@beatfitnessgames.com>
2021-01-24 19:17:56 +0100

    Added Quaternions

---
+++ Quaternion.cpp
@@ -0,0 +1,53 @@
+//
+// Created by cleme on 24/01/2021.
+//
+
+#include "Quaternion.h"
+
+double Quaternion::norm() const {
+    return x*x + y*y + z*z + w*w;
+}
+
+void Quaternion::normalize() {
+    double normValue = norm();
+    x /= normValue;
+    y /= normValue;
+    z /= normValue;
+    w /= normValue;
+}
+
+Quaternion Quaternion::normalized() const {
+    Quaternion copy{*this};
+    copy.normalize();
+    return copy;
+}
+
+Quaternion::operator Vector() const {
+    return Vector(x, y, z);
+}
+
+Vector Quaternion::applyRotation(const Vector &input) {
+    Quaternion rotation{*this};
+    Quaternion rotationConjugate{this->conjugate()};
+    Quaternion objectToRotate{input};
+
+    return Vector(rotationConjugate * objectToRotate * rotation);
+}
+
+Quaternion Quaternion::conjugate() const {
+    return Quaternion(-x, -y, -z, w);
+}
+
+Quaternion& operator*=(Quaternion& q1, const Quaternion& q2) {
+    q1.x *= q2.x;
+    q1.y *= q2.y;
+    q1.z *= q2.z;
+    q1.w *= q2.w;
+    return q1;
+}
+
+Quaternion operator*(const Quaternion& q1, const Quaternion& q2) {
+    Quaternion output{q1};
+    output *= q2;
+    return output;
+}
---
+++ Quaternion.h
@@ -0,0 +1,37 @@
+//
+// Created by cleme on 24/01/2021.
+//
+
+#ifndef RAYTRACER_QUATERNION_H
+#define RAYTRACER_QUATERNION_H
+
+#include "triple.h"
+
+class Quaternion {
+public:
+    double x, y, z, w; // Q = xi + yj + zk + w
+
+    Quaternion(double x, double y, double z, double w)
+        : x(x), y(y), z(z), w(w)
+    { }
+
+    Quaternion() : Quaternion(0, 0, 0, 0)
+    { }
+
+    explicit Quaternion(const Vector& v) : Quaternion(v.X(), v.Y(), v.Z(), 0)
+    { }
+
+    [[nodiscard]] double norm() const;
+    void normalize();
+    [[nodiscard]] Quaternion normalized() const;
+    [[nodiscard]] Quaternion conjugate() const;
+
+    explicit operator Vector() const;
+
+    Vector applyRotation(const Vector& input);
+};
+
+Quaternion& operator*=(Quaternion&, const Quaternion&);
+Quaternion operator*(const Quaternion&, const Quaternion&);
+
+#endif //RAYTRACER_QUATERNION_H
