55b01e452f9ab23ea7f30c84596b690e562dee8d
Clément Couture <clement.couture@beatfitnessgames.com>
2021-02-05 10:55:58 +0100

    missed that in the last commit

--- box.h
+++ box.h
@@ -5,18 +5,50 @@
 #ifndef QUAD_H_115209AE
 #define QUAD_H_115209AE
 
+#include <unordered_map>
 #include "object.h"
+#include "Plane.h"
+
+class Quadrilateral : public Object {
+public:
+
+    Quadrilateral(const Point& Position, const Vector& Up, const Vector& Side)
+        : Position(Position), Up(Up), Side(Side)
+    { }
+
+    [[nodiscard]] Hit intersect(const Ray &ray) const override;
+    [[nodiscard]] std::array<double, 2> getTextureCoordinatesFor(const Point &) const override;
+
+    const Point Position;
+    const Vector Up;
+    const Vector Side;
+
+private:
+
+    const Plane ownPlane{Position, getThirdOrthogonalVector(Side, Up)};
+
+    // Used in UV calculations
+    const double sideComponentFactor = 1 / ownPlane.Normal.dot(Side.cross(Up));
+    const double upComponentFactor = 1  / ownPlane.Normal.dot(Up.cross(Side));
+};
 
 class Box : public Object {
 public:
-    Box(Point Position, float width, float depth, float height) : Position(Position), width(width), depth(depth), height(height)  { }
+    Box(const Point& Position, const Vector& Up, const Vector& Side, float Depth) : Faces(computeFaces(Position, Up, Side, Depth))  { }
 
-    Hit intersect(const Ray &ray) const override;
+    [[nodiscard]] Hit intersect(const Ray &ray) const override;
+    [[nodiscard]] std::array<double, 2> getTextureCoordinatesFor(const Point &) const override;
 
-    const Point Position;
-    const float width;
-    const float depth;
-    const float height;
+    const std::array<Quadrilateral, 6> Faces;
+
+private:
+    static std::array<Quadrilateral, 6> computeFaces(const Point &vector,
+                                                    const Vector &vector1,
+                                                    const Vector &vector2,
+                                                    float depth);
+
+
+    static std::unordered_map<Point, const Quadrilateral&> parentOf;
 };
 
 
