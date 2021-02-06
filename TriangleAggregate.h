51b84f9e470c3abb29e9de3db29c045080f118e1
Clément Couture <clement.couture@beatfitnessgames.com>
2021-02-06 11:22:15 +0100

    Fixed the model loading to include files without normal data

--- TriangleAggregate.h
+++ TriangleAggregate.h
@@ -25,7 +25,7 @@ public:
             : triangles(std::move(triangles))
     { }
 
-    TriangleAggregate(std::string fileName) : TriangleAggregate(objParsing(fileName))
+    TriangleAggregate(const std::string& fileName) : TriangleAggregate(objParsing(fileName))
     { }
 
     static std::vector<Triangle> objParsing(std::string fileName){
@@ -36,25 +36,41 @@ public:
         GLMmodel *model = glmReadOBJ(fName);
         free(fName);
 
+        if (model->numnormals == 0 && model->numfacetnorms == 0)
+            glmFacetNormals(model);
+
         std::vector<Triangle> triangles{};
 
         for(int i = 0; i < model->numtriangles; i++){
 
             int* vertexIndices = model->triangles[i].vindices;
             int* normalIndices = model->triangles[i].nindices;
-            int* uvIndices = nullptr;
-            if (model->numtexcoords > 0) {
-                uvIndices = model->triangles[i].vindices;
-            }
+            int* uvIndices = model->triangles[i].vindices;
+
             std::vector<Vertex> vertices{};
 
             for (int j = 0; j < 3; ++j) {
-                vertices.push_back(Vertex{
-                    Point{model->vertices[vertexIndices[j]*3], model->vertices[vertexIndices[j]*3 + 1], model->vertices[vertexIndices[j]*3 + 2]},
-                    Vector{model->normals[normalIndices[j]*3], model->normals[normalIndices[j]*3+1], model->normals[normalIndices[j]*3+2]},
-                    (uvIndices ? std::array<double, 2>{model->texcoords[uvIndices[j]*2], model->texcoords[uvIndices[j]*2+1]} : std::array<double, 2>{0, 0})
-                });
+
+                Point position{model->vertices[vertexIndices[j]*3], model->vertices[vertexIndices[j]*3 + 1], model->vertices[vertexIndices[j]*3 + 2]};
+
+                Vector normal{};
+                if (model->numnormals > 0)
+                    normal = Vector{model->normals[normalIndices[j]*3], model->normals[normalIndices[j]*3+1], model->normals[normalIndices[j]*3+2]};
+                else if (model->numfacetnorms > 0)
+                    // facet normals are calculated using counter clockwise vertex order. That is why we use the opposite of the facet normals
+                    normal = Vector{model->facetnorms[(i+1)*3], model->facetnorms[(i+1)*3+1], model->facetnorms[(i+1)*3+2]};
+
+                std::array<double, 2> UV{0, 0};
+                if (model->numtexcoords > 0)
+                    UV = {model->texcoords[uvIndices[j]*2], model->texcoords[uvIndices[j]*2+1]};
+
+                vertices.emplace_back(
+                    position,
+                    normal,
+                    UV
+                );
             }
+
             triangles.emplace_back(
                 vertices[0],
                 vertices[1],
